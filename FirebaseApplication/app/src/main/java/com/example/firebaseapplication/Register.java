f5a909c2f74f4450c74a4443f0ad065ac7f58560
Zion Jiam <zionjiam88.18@gmail.com>
2019-12-13 23:01:17 +0800

    Added Firebase Auth

--- FirebaseApplication/app/src/main/java/com/example/firebaseapplication/Register.java
+++ FirebaseApplication/app/src/main/java/com/example/firebaseapplication/Register.java
@@ -18,28 +18,41 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
-    EditText mEmail,mPassword;
-    TextView mRegisterBtn;
-    Button mLoginBtn;
+    EditText mFullname,mEmail,mPassword,mPhone;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
     FirebaseAuth fAuth;
+    ProgressBar mProgressBar;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.register);
+
+        mFullname = findViewById(R.id.fullname);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mPhone = findViewById(R.id.phone);
+        mRegisterBtn = findViewById(R.id.register);
+        mLoginBtn = findViewById(R.id.login);
+        mProgressBar = findViewById(R.id.progressBarRegister);
 
         // Get instance of firebase authentication database
         fAuth = FirebaseAuth.getInstance();
 
-        mEmail = findViewById(R.id.email);
-        mPassword = findViewById(R.id.password);
-        mLoginBtn = findViewById(R.id.login);
-        mRegisterBtn = findViewById(R.id.register);
 
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+        // go straight to main page if user has not log out
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = mEmail.getText().toString().trim();
@@ -59,27 +72,27 @@ public class Login extends AppCompatActivity {
                     mPassword.setError("Password Must be more than 6 characters");
                 }
 
+                mProgressBar.setVisibility(View.VISIBLE);
 
-                // Authenticate and Login User
-                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                //Register User
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
-                            Toast.makeText(Login.this,"Login Successful", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Register.this,"User is created", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         }else{
-                            Toast.makeText(Login.this,"Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Register.this,"Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
         });
 
-
-        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),Register.class));
+                startActivity(new Intent(getApplicationContext(),Login.class));
             }
         });
 
